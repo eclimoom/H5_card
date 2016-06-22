afa8d9d5704aa390636f7a661af91cefeec4c864
hejunji <hejj@wohai.com>
2016-06-22 12:16:42 +0800

    reset base

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# H5_card
+LotteryCard
+=======
+H5刮奖,
+
+need support canvas
+
+##使用说明
+
+var card = new CardCover(
+        {
+            id: 'cardContainer',
+            coverImg: 'images/cover.png',
+            width:300,
+            height:144,
+            percentClear:80,
+            drawPercentCallback: drawPercent
+        }
+);
+card.init();
+
+function drawPercent(percent) {
+    drawPercentNode.innerHTML = percent + '%';
+}
+
+##构造方法参数解释
+
+`id`:刮奖容器,必须
+
+`coverImg`:涂层内容
+
+`width`:刮奖区域宽度,默认为300px,可空
+
+`height`:刮奖区域高度,默认为100px,可空
+
+`percentClear`:刮开的区域百分比 达到多少清空涂层,默认100,可空
+
+`drawPercentCallback`:刮开的区域百分比回调函数,可空
+
+
---
+++ libs/CardCover.js
@@ -0,0 +1,132 @@
+function CardCover(param) {
+    this.conNode = document.getElementById(param.id);
+    this.coverImg = param.coverImg;
+    this.mask = null;
+    this.maskCtx = null;
+    this.percentClear = param.percentClear || 100;
+    this.width = param.width || 300;
+    this.height = param.height || 100;
+    this.clientRect = null;
+    this.drawPercentCallback = param.drawPercentCallback;
+}
+
+CardCover.prototype = {
+    createElement: function (tagName, attributes) {
+        var ele = document.createElement(tagName);
+        for (var key in attributes) {
+            ele.setAttribute(key, attributes[key]);
+        }
+        return ele;
+    },
+    getTransparentPercent: function(ctx, width, height) {
+        var imgData = ctx.getImageData(0, 0, width, height),
+            pixles = imgData.data,
+            transPixs = [],percent=0;
+        for (var i = 0, j = pixles.length; i < j; i += 4) {
+            var a = pixles[i + 3];
+            if (a < 128) {
+                transPixs.push(i);
+            }
+        }
+        percent = (transPixs.length / (pixles.length / 4) * 100).toFixed(2);
+        if(percent>this.percentClear){
+            this.clearCanvas(ctx);
+        }
+        return percent;
+    },
+    clearCanvas:function(ctx){
+        ctx.clearRect(0, 0, this.width, this.height);
+    },
+    resizeCanvas: function (canvas, width, height) {
+        canvas.width = width;
+        canvas.height = height;
+        console.log(width,height);
+        canvas.getContext('2d').clearRect(0, 0, width, height);
+    },
+    drawPoint: function (x, y) {
+        this.maskCtx.beginPath();
+        var radgrad = this.maskCtx.createRadialGradient(x, y, 0, x, y, 30);
+        radgrad.addColorStop(0, 'rgba(0,0,0,0.6)');
+        radgrad.addColorStop(1, 'rgba(255, 255, 255, 0)');
+        this.maskCtx.fillStyle = radgrad;
+        this.maskCtx.arc(x, y, 30, 0, Math.PI * 2, true);
+        this.maskCtx.fill();
+        if (this.drawPercentCallback) {
+            this.drawPercentCallback.call(null, this.getTransparentPercent(this.maskCtx, this.width, this.height));
+        }
+    },
+    bindEvent: function () {
+        var _this = this;
+        var device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+        var clickEvtName = device ? 'touchstart' : 'mousedown';
+        var moveEvtName = device? 'touchmove': 'mousemove';
+        if (!device) {
+            var isMouseDown = false;
+            document.addEventListener('mouseup', function(e) {
+                isMouseDown = false;
+            }, false);
+        } else {
+            document.addEventListener("touchmove", function(e) {
+                if (isMouseDown) {
+                    e.preventDefault();
+                }
+            }, false);
+            document.addEventListener('touchend', function(e) {
+                isMouseDown = false;
+            }, false);
+        }
+        this.mask.addEventListener(clickEvtName, function (e) {
+            isMouseDown = true;
+            var docEle = document.documentElement;
+            if (!_this.clientRect) {
+                _this.clientRect = {
+                    left: 0,
+                    top:0
+                };
+            }
+            var x = (device ? e.touches[0].clientX : e.clientX) - _this.clientRect.left + docEle.scrollLeft - docEle.clientLeft;
+            var y = (device ? e.touches[0].clientY : e.clientY) - _this.clientRect.top + docEle.scrollTop - docEle.clientTop;
+            _this.drawPoint(x, y);
+        }, false);
+
+        this.mask.addEventListener(moveEvtName, function (e) {
+            if (!device && !isMouseDown) {
+                return false;
+            }
+            var docEle = document.documentElement;
+            if (!_this.clientRect) {
+                _this.clientRect = {
+                    left: 0,
+                    top:0
+                };
+            }
+            var x = (device ? e.touches[0].clientX : e.clientX) - _this.clientRect.left + docEle.scrollLeft - docEle.clientLeft;
+            var y = (device ? e.touches[0].clientY : e.clientY) - _this.clientRect.top + docEle.scrollTop - docEle.clientTop;
+            _this.drawPoint(x, y);
+        }, false);
+    },
+    drawMask: function() {
+        this.resizeCanvas(this.mask, this.width, this.height);
+        var image = new Image(),
+            _this = this;
+        image.onload = function () {
+            _this.maskCtx.drawImage(this, 0, 0,_this.width,_this.height);
+            _this.maskCtx.globalCompositeOperation = 'destination-out';
+        }
+        image.src = this.coverImg;
+    },
+    init: function () {
+        this.mask = this.mask || this.createElement('canvas', {
+                style: 'position:absolute;left:0;top:0;'
+            });
+
+        if (!this.conNode.innerHTML.replace(/[\w\W]| /g, '')) {
+            this.conNode.appendChild(this.mask);
+            this.clientRect = this.conNode ? this.conNode.getBoundingClientRect() : null;
+            this.bindEvent();
+        }
+
+        this.maskCtx = this.maskCtx || this.mask.getContext('2d');
+        this.drawMask();
+    }
+}
